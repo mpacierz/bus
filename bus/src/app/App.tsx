8fefcf0de10bfdc2f448e4f3e79d0e2689421ea9
mpacierz <mpacierz@agh.edu.pl>
2024-12-29 12:51:25 +0100

    initial

---
+++ bus/src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import StartPage from './components/StartPage/StartPage';
+
+const App = () => {
+    return (
+        <div className='bus-app'>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<StartPage />} />
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
---
+++ bus/src/app/components/StartPage/StartPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const StartPage: React.FC = () => {
+    return (
+        <div className='bus-app-page'>
+            <Link to='/'>HP5 test</Link>
+        </div>
+    );
+}
+
+export default StartPage;
